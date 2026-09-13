e70217f3ed4377b1ee243b057a231be6a7fc5e1c
robisonacquashop-ui <robison@bisonx.local>
2026-09-12 22:35:56 -0300

    Libera download do cliente Teste_Completo (codigo BX...) em 12/09/2026

--- download/codigos.js
+++ download/codigos.js
@@ -1,4 +1,4 @@
 // Lista de hashes (prefixo 8) de codigos de liberacao ATIVOS.
 // NAO contem os codigos em si - impossivel reverter.
-var CODIGOS_ATIVOS = ["9723c191","fcce73f3","3385b051"];
-// Gerado por liberar_download.py em 12/09/2026 (fonte: Teste Centro)
+var CODIGOS_ATIVOS = ["3385b051","9723c191","fcce73f3","72bd4b84"];
+// Gerado por liberar_download.py em 12/09/2026 (fonte: Teste_Completo)
